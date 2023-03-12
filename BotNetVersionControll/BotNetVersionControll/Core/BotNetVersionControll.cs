92c495ddbd2015c91cf447f16cb33b7a396e96c0
Tomsztyl <tomek.jelito@gmail.com>
2023-03-12 21:35:36 +0100

    Added downloading version from server

--- BotNetVersionControll/BotNetVersionControll/Core/BotNetVersionControll.cs
+++ BotNetVersionControll/BotNetVersionControll/Core/BotNetVersionControll.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 using System.Net;
 using System.Runtime.InteropServices;
 using Newtonsoft.Json;
@@ -11,26 +12,34 @@ namespace BotNetVersionControll.Core
 
         public static Process BotProccesRun = new Process();
         public static BotProperties BotPropertiesData { get; private set; } = new BotProperties();
+        public static bool VersionUpdating { get; private set; } = false;
 
         #region Behaviour App
 
         static void Main( string[] args )
         {
-            AppDomain.CurrentDomain.ProcessExit += new EventHandler( OnExitApp );
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler( TaskTry );
             new BotNetVersionControll().InitializeBot().GetAwaiter().GetResult();
         }
 
-        private static void OnExitApp( object sender, EventArgs e )
+        private static async void TaskTry( object sender, EventArgs e )
         {
-            TryKillBotAssisant();
-            OnSaveApp();
+            await OnExitApp();
+        }
+
+        private static async Task OnExitApp()
+        {
+            AppDomain.CurrentDomain.ProcessExit -= new EventHandler( TaskTry );
+            await TryKillBotAssisant();
+            await OnSaveApp();
             Debuger.PrintLog( "EXIT!", ETypeLog.Warning );
             Thread.Sleep( 1000 );
         }
 
-        private static void OnSaveApp()
+        private static async Task OnSaveApp()
         {
             Debuger.PrintLog( "SAVE APP!", ETypeLog.Warning );
+            await SavePropertiesBot();
         }
 
         #endregion
@@ -42,8 +51,32 @@ namespace BotNetVersionControll.Core
             {
                 return;
             }
-            TryRunnProcess();
+            await TryRunnProcess();
             await UpdateFeatching();
+        }
+
+        private static async Task SavePropertiesBot()
+        {
+            string prefix = string.Empty;
+            if( RuntimeInformation.IsOSPlatform( OSPlatform.Linux ) )
+            {
+                prefix = "/";
+            }
+            else
+            {
+                prefix = "\\";
+            }
+
+            string path = Directory.GetCurrentDirectory();
+            string formatPath = string.Format( "{0}{1}{2}", path, prefix, NAME_FILE_DATA );
+            File.Delete( @formatPath );
+
+            FileStream fileCreated = File.Create( @formatPath );
+            fileCreated.Close();
+            string json = JsonConvert.SerializeObject( BotPropertiesData, Formatting.Indented );
+            File.WriteAllText( @formatPath, json );
+            string pathCreated = string.Format( "[{0}] UPDATE FILE DATA PATH: {1}", NAME_FILE_DATA, @formatPath );
+            Debuger.PrintLog( pathCreated, ETypeLog.Succes );
         }
 
         private static bool TryLoadFileData()
@@ -87,29 +120,84 @@ namespace BotNetVersionControll.Core
 
         public async Task UpdateFeatching()
         {
-            await Task.Delay( BotPropertiesData.TimeUpdateFiles * 1000 );
             TryUpdateFeatching();
+            await Task.Delay( BotPropertiesData.TimeUpdateFiles * 1000 );
             await UpdateFeatching();
         }
 
         private void TryUpdateFeatching()
         {
+            if( VersionUpdating || !BotPropertiesData.FeatchingCurrentVersion )
+            {
+                return;
+            }
+
             string versionFromServer = GetVerstionFromServer();
+
             if( versionFromServer != BotPropertiesData.BotAssistantVersion )
             {
-
+                Debuger.PrintLog( string.Format( "Latest version found, starting update: [{0}] to [{1}] "
+                    , BotPropertiesData.BotAssistantVersion, versionFromServer ), ETypeLog.Warning );
+                VersionUpdating = true;
+                TryDonwloadVersion( versionFromServer );
             }
         }
 
-        private static bool TryKillBotAssisant()
+        private void TryDonwloadVersion( string version )
         {
-            if( BotProccesRun != null )
+            string urlDonwload = string.Format( "{0}{1}.zip", BotPropertiesData.LinkArchiveMainPath, version );
+            string localPath = string.Format( "{0}bot_version_{1}.zip", BotPropertiesData.MainDirectoryBotPath, version );
+            Uri siteUri = new Uri( urlDonwload );
+            using( var client = new WebClient() )
             {
-                Debuger.PrintLog( "Kill " + BotProccesRun, ETypeLog.Warning );
-                BotProccesRun.Kill();
-                return true;
+                client.DownloadProgressChanged += HandleDownloadProgressChanged;
+                client.DownloadFileCompleted += HandleDonwloadFileCompleted;
+                client.DownloadFileAsync( siteUri, localPath );
             }
-            return false;
+        }
+
+        private void HandleDonwloadFileCompleted( object sender, AsyncCompletedEventArgs e )
+        {
+            VersionUpdating = false;
+
+            if( e.Cancelled )
+            {
+                Debuger.PrintLog( "File download cancelled.", ETypeLog.Error );
+            }
+
+            if( e.Error != null )
+            {
+                Debuger.PrintLog( e.Error.ToString(), ETypeLog.Error );
+            }
+
+            if( !e.Cancelled && e.Error == null )
+            {
+                Debuger.PrintLog( "File downloaded!", ETypeLog.Succes );
+                BotPropertiesData.BotAssistantVersion = GetVerstionFromServer();
+            }
+        }
+
+        private void HandleDownloadProgressChanged( object sender, DownloadProgressChangedEventArgs e )
+        {
+            string formatProgress = string.Format( "{0}    downloaded {1} of {2} bytes. {3} % complete...",
+                (string)e.UserState,
+                e.BytesReceived,
+                e.TotalBytesToReceive,
+                e.ProgressPercentage );
+            Debuger.PrintLog( formatProgress );
+        }
+
+        private static async Task TryKillBotAssisant()
+        {
+            ProcessStartInfo processStartInfo = BotProccesRun.StartInfo;
+            if( !string.IsNullOrEmpty( processStartInfo.FileName ) )
+            {
+                if( !BotProccesRun.HasExited )
+                {
+                    Debuger.PrintLog( "Kill " + BotProccesRun, ETypeLog.Warning );
+                    BotProccesRun.Kill();
+                }
+            }
         }
 
         private string GetVerstionFromServer()
@@ -126,9 +214,9 @@ namespace BotNetVersionControll.Core
             }
         }
 
-        private void TryRunnProcess()
+        private async Task TryRunnProcess()
         {
-            BotProccesRun.StartInfo.FileName = BotPropertiesData.PathFilesBot;
+            BotProccesRun.StartInfo.FileName = BotPropertiesData.PathFilesBotRun;
             BotProccesRun.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
             BotProccesRun.Start();
         }
--- BotNetVersionControll/BotNetVersionControll/Core/BotProperties.cs
+++ BotNetVersionControll/BotNetVersionControll/Core/BotProperties.cs
@@ -2,10 +2,12 @@
 {
     internal class BotProperties
     {
-        public string LinkVersion { get; set; } = "http://192.168.1.166/VersionFile.txt";
+        public string LinkVersion { get; set; } = "http://192.168.1.166/VersionsZip/VersionFile.txt";
+        public string LinkArchiveMainPath { get; set; } = "http://192.168.1.166/VersionsZip/";
         public int TimeUpdateFiles { get; set; } = 7;
-        public string LinkFileVersion { get; set; } = "";
-        public string PathFilesBot { get; set; } = "D:\\ProjectsApp\\BotNet\\BotAssistant_Net\\BotAssistant_Net\\bin\\Debug\\net6.0\\BotAssistant_Net.exe";
+        public string PathFilesBotRun { get; set; } = "D:\\ProjectsApp\\BotNet\\BotAssistant_Net\\BotAssistant_Net\\bin\\Debug\\net6.0\\BotAssistant_Net.exe";
         public string BotAssistantVersion { get; set; } = "0.0.1";
+        public string MainDirectoryBotPath { get; set; } = "D:\\ProjectsApp\\BotNet\\BotAssistant_Net\\BotAssistant_Net\\bin\\Debug\\";
+        public bool FeatchingCurrentVersion { get; set; } = true;
     }
 }
